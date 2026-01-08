af28e0d62a32a8915a1fba8d2750c68f2d6dc0c3
AlexBrunenberg <BrAl161208@leerling.mosa-rt.be>
2026-01-08 09:12:49 +0100

    8/1/26

--- oefeningen/dht11ledPWM.py
+++ oefeningen/dht11ledPWM.py
@@ -4,6 +4,47 @@ from gpiozero import RGBLED, PWMLED # importeer modules
 led = RGBLED(red = PWMLED(9), green = PWMLED(10), blue = PWMLED(11)) # definieer de LED
 dhtDevice = adafruit_dht.DHT11(board.D18)   #verbind de dht11 op pin 18 en noem het dhtDevice
 lijst = [[255,0,0],[205,38,38],[205,55,0],[250,128,114],[238,149,114],[255,127,0],[205,133,0]]
+lijst = [255,0,0 
+
+205,38,38 
+
+205,55,0 
+
+250,128,114 
+
+238,149,114 
+
+255,127,0 
+
+205,133,0 
+
+238,173,14 
+
+238,221,130 
+
+205,205,0 
+
+255,246,143 
+
+124,252,0 
+
+0,255,0 
+
+78,238,148 
+
+0,245,255 
+
+151,255,255 
+
+0,255,255 
+
+135,206,250 
+
+0,191,255 
+
+30,144,255 
+
+0,0,255 ]
 
 while True:
     temperatuur = dhtDevice.temperature
