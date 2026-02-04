46baa2b856d4176e87919b8e260ec3b31049687d
AlexBrunenberg <BrAl161208@leerling.mosa-rt.be>
2026-02-04 10:14:28 +0100

    auto update 2026-02-04 10:14:28

--- oefeningen/DHT11Cloud.py
+++ oefeningen/DHT11Cloud.py
@@ -3,8 +3,8 @@ import time,board,adafruit_dht, subprocess #importeer
 
 dhtDevice = adafruit_dht.DHT11(board.D18)   #verbind de dht11 op pin 18 en noem het dhtDevice
 
-temperature_c = dhtDevice.temperature   #zet de temperatuur van de DHT11 in variabele temperature_c
-luchtvochtigheid = dhtDevice.humidity   #zet de luchtvochtigheid van de DHT11 in variabele luchtvochtigheid
+#temperature_c = dhtDevice.temperature   #zet de temperatuur van de DHT11 in variabele temperature_c
+#luchtvochtigheid = dhtDevice.humidity   #zet de luchtvochtigheid van de DHT11 in variabele luchtvochtigheid
 
 f=open("DHT11Cloud.csv","w")    #start met schrijven in het bestand DHT11Cloud.csv
 f.write("temp,lv \n")   #schrijf in het bestand en maak een nieuwe regel
@@ -16,7 +16,7 @@ while True: #doe de hele tijd
             temperature_c = dhtDevice.temperature   #zet de temperatuur van de DHT11 in variabele temperature_c
             luchtvochtigheid = dhtDevice.humidity   #zet de luchtvochtigheid van de DHT11 in variabele luchtvochtigheid
             print(temperature_c, luchtvochtigheid)
-            f=open("thermometerCSV.csv","a")    # voeg toe aan het bestand
+            f=open("DHT11Cloud.csv","a")    # voeg toe aan het bestand
             f.write(f"{temperature_c},{luchtvochtigheid}\n") #tekst schrijven in het bestand
             f.close()   #sluit het bestand
             subprocess.run(["/bin/bash", "/bin/auto_push.sh"])
