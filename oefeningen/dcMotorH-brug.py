0fd9a0375f778df1143a28c13fe0479de87a7db5
AlexBrunenberg <BrAl161208@leerling.mosa-rt.be>
2026-03-12 10:40:44 +0100

    code dcmotorH-brug klaar

--- oefeningen/dcMotorH-brug.py
+++ oefeningen/dcMotorH-brug.py
@@ -1,12 +1,9 @@
-from gpiozero import Motor, MCP3008
-from time import sleep
+from gpiozero import Motor, MCP3008 # importeer modules
+from time import sleep  # importeer module
 
-pot = MCP3008(channel=0)
-motor = Motor(forward=4, backward=14)
-while True:
-    motor.forward()
-    print(pot.value)
-    sleep(5)
-    motor.backward()
-    print(pot.value)
-    sleep(5)
+pot = MCP3008(channel=0)    # maak de potentiometer gelijk aan channel 0 van de MCP3008
+motor = Motor(forward=17, backward=14, pwm=True)    # stel de pinnen in voor de motor om naar voor of achter te gaan en zet pwm aan
+while True: # doe altijd
+    motor.forward(pot.value)    # laat de motor vooruit draaien op de snelheid van de waarde van de potentiometer
+    print(pot.value)    # print de waarde van de potentiometer
+    sleep(1)    # wacht 1 seconde
